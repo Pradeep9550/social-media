b9adc24b6472f45e23272993694f342bf3546079
Pradeep9550 <kapil95550@gmail.com>
2025-08-24 22:59:03 +0530

    social-media

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': 'https://social-media-3qeu.onrender.com'
     }
   }
 })
